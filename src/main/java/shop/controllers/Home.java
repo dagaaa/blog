5f25def64dae24f8df25eb81f36ea697f2f04f97
dagaaa <dagmaragoj@gmail.com>
2018-02-28 13:54:38 +0100

    layout

---
+++ src/main/java/shop/controllers/Home.java
@@ -0,0 +1,15 @@
+package shop.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Home {
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+
+}
